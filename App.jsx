eea2954805f2838bef318728e064577a94d3fe81
LucaKSpiller <lukspiller01@gmail.com>
2023-05-09 11:27:11 -0300

    Instalação de dependências, correção de erros e atualização projeto

--- App.jsx
+++ App.jsx
@@ -1,13 +1,10 @@
-
-import { Text, View } from 'react-native';
-
-
+import React from 'react';
+import {Router} from "./src/screens/routes"
 
 export default function App() {
+  
   return (
-    <View style={styles.container}>
-      <Text>Teste!</Text>
-    </View>
+    <Router/>
   );
 }
 
